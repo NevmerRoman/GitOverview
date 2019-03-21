aa0a2f6ca9e94693715a06ad036521b06f9c41bb
NevmerRoman <nevmer_roman@mail.ru>
2019-03-21 19:19:58 +0300

    Задание по Си

--- Project/c_lang_task/c_lang_task.c
+++ Project/c_lang_task/c_lang_task.c
@@ -3,10 +3,13 @@
 #include <string.h>
 #include "c_lang_task.h"
 
+
 void print_number_table(short col, short row) {
+    printf("\n    Task 1 \n");
     srand(time(NULL));
     short **tab;
     tab = (short**)malloc(row * sizeof(short*));
+
     for (int i = 0; i < row; i++){
         tab[i] = (short*)malloc(col * sizeof(short));
         for (int j = 0; j < col; j++){
@@ -17,56 +20,159 @@ void print_number_table(short col, short row) {
     }
 }
 
+
 void write_nils_to_head_file(const char* full_fname1, int N) {
+    printf("\n    Task 2 \n");
     FILE *file;
     file = fopen(full_fname1,"w");
     char ch =0x00;
+
     for (int i = 0; i < N; i++){
         fputc (ch, file);
     }
     fclose(file);
 }
 
+
 void write_ones_to_tail_fail(const char* full_fname1, int N) {
+    printf("\n    Task 3 \n");
     FILE *file;
     file = fopen(full_fname1,"a");
     char ch =0xff;
+
     for (int i = 0; i < N; i++){
         fputc (ch, file);
     }
     fclose(file);
 }
 
-int read_i_string_from_file(const char* full_fname2, int i, char* cut_str, int max_sz) {
+
+int read_i_string_from_file(const char* full_fname2, int i, char* out_str, int max_sz) {
     FILE *file;
     file = fopen(full_fname2,"r");
-    for (int j = 0; j < i; j++){
 
+    if (file == NULL){
+        printf("ERROR\n File not found.\n");
+        return -1;
+    }
+
+    char ch = 'a';
+    int j = 1;
+    while (j < i){
+        if (feof(file)){
+            printf("ERROR\nThere is no such line.\n");
+            return -1;
+        }
+        ch = fgetc(file);
+        if (ch == '\n'){
+            j++;
+        }
+    }
+
+    int flag = 0;
+    while ((flag < max_sz) & (!feof(file)) & (out_str[flag-1] !='\n')){
+        out_str[flag] = fgetc(file);
+        flag++;
+    }
+
+    if ((flag == max_sz) & (out_str[flag - 1] != '\n')){
+        out_str[flag-1] = '\0';
     }
     fclose(file);
+    return flag;
 }
-/*
-void TEST_memcpy_and_memset(void){
 
+
+void TEST_memcpy_and_memset(void){
+    printf("\n    Task 5 \n");
+    int si = 40*sizeof(int);
+    int sd = 20*sizeof(double);
+    int sc = 80*sizeof(char);
+
+    int *in = (int*)malloc(si);
+    double *doubl = (double*)malloc(sd);
+    char *cha = (char*)malloc(sc);
+
+    memset(cha,0x01,sc);
+    memset(doubl,0x00,sd);
+    memset(in,0x00,si);
+    for (int i = 0; i < 40; i++){
+        memset(in+i,0x01,1);
+    }
+
+    for (int i = 0; i <10; i++){
+        printf("%c ",cha[i]);
+    }
+    printf("\n");
+    for (int i = 0; i <10; i++){
+        printf("%d ",in[i]);
+    }
+    printf("\n");
+    for (int i = 0; i <10; i++){
+        printf("%f ",doubl[i]);
+    }
+    printf("\n");
+
+    memcpy(cha, in, sc);
+    memcpy(in, doubl, si);
+    memcpy(doubl, cha, sc);
+
+    for (int i = 0; i <10; i++){
+        printf("%c ",cha[i]);
+    }
+    printf("\n");
+    for (int i = 0; i <10; i++){
+        printf("%d ",in[i]);
+    }
+    printf("\n");
+    for (int i = 0; i <10; i++){
+        printf("%f ",doubl[i]);
+    }
+    printf("\n");
 }
+
 
 void TEST_read_i_string_from_file(void) {
+    printf("\n    Task 6 and 4 \n");
+    char* full_fname2 = "/home/student/Project/c_lang_task/File2.Txt";
 
-}*/
+    int i = 6, max_sz = 15;
+    char* out_str = (char*)malloc(sizeof(char)*max_sz);
+    int size_str = read_i_string_from_file(full_fname2, i, out_str, max_sz);
+    printf("%d\n", size_str);
+    for (int j = 0; j < max_sz; j++){
+        printf("%c", out_str[j]);
+    }
+    printf("\n");
+
+    free(out_str);
+    max_sz = 7;
+    out_str = (char*)malloc(sizeof(char)*max_sz);
+    size_str = read_i_string_from_file(full_fname2, i, out_str, max_sz);
+    printf("%d\n", size_str);
+    for (int j = 0; j < max_sz; j++){
+        printf("%c", out_str[j]);
+    }
+    printf("\n");
+
+    i = 20;
+    size_str = read_i_string_from_file(full_fname2, i, out_str, max_sz);
+    printf("%d\n", size_str);
+    for (int j = 0; j < max_sz; j++){
+        printf("%c", out_str[j]);
+    }
+    printf("\n");
+}
+
 
 int main() {
-//int col = 20, row = 20;
-//char* full_fname1 = "/home/student/Project/c_lang_task/File.Txt";
-char* full_fname2 = "/home/student/Project/c_lang_task/File2.Txt";
-char* cut_str;
-//int N = 8;
-int i = 6, max_sz = 11;
-//print_number_table(col, row);
-//write_nils_to_head_file(full_fname, N);
-//write_ones_to_tail_fail(full_fname, N);
-read_i_string_from_file(full_fname, i, cut_str, max_sz);
-/*TEST_memcpy_and_memset();
-TEST_read_i_string_from_file();*/
-system("pause");
+int col = 4, row = 8;
+char* full_fname1 = "/home/student/Project/c_lang_task/File.Txt";
+int N = 8;
+print_number_table(col, row);
+write_nils_to_head_file(full_fname1, N);
+write_ones_to_tail_fail(full_fname1, N);
+TEST_memcpy_and_memset();
+TEST_read_i_string_from_file();
 return 0;
 }
